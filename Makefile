ea56d30ab57d9ef8fc6508905a0c41f3906c12b1
th3worst4 <caiocouto25@hotmail.com>
2024-01-17 13:08:40 -0300

    minor changes

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 CC = g++
 CFLAGS = -m64 $$(pkg-config --cflags --libs Qt5Widgets qtermwidget5) -fPIC
 
-CFILES = my-term.cpp
-OBJECTS = my-term.o
-DOBJECTS = dmy-term.o
-BIN = my-term
-DBIN = dmy-term
+CFILES = neofetchwidget.cpp
+OBJECTS = neofetchwidget.o
+BIN = neofetchwidget
 
 $(OBJECTS): %.o: src/%.cpp
 	$(CC) $(CFLAGS) -o bin/$@ -c $^
--- src/neofetchwidget.cpp
+++ src/neofetchwidget.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char **argv){
     QApplication app(argc, argv);
      
-    QWidget *widget = new QWidget(NULL, Qt::Dialog | Qt::FramelessWindowHint);
+    QWidget *widget = new QWidget(NULL, Qt::Tool | Qt::FramelessWindowHint);
+    widget->setAttribute(Qt::WA_QuitOnClose);
     widget->resize(600, 400);
     widget->move(766, 0);
     widget->setWindowOpacity(0.7);
@@ -22,10 +23,9 @@ int main(int argc, char **argv){
     console->setGeometry(0, 0, 600, 400);
     console->setTerminalFont(font); 
     console->setColorScheme("GreenOnBlack");
+    console->sendText("cd ~/Desktop && clear\r");
     console->sendText("neofetch\r");
-
-    //QEvent *exitConsole = new QEvent(QEvent::Close);
-    
+ 
     QObject::connect(console, SIGNAL(finished()), widget, SLOT(close()));
     
 
